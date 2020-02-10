766e6ff43831717cf7c8a6289f3ca7f61c4f3484
Arkaitz Osa <arkaitz@arima.eu>
2020-02-10 09:16:26 +0100

    rebuilding site lunes, 10 de febrero de 2020, 09:16:26 CET

--- deploy.sh
+++ deploy.sh
@@ -8,10 +8,6 @@ printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 # Build the project.
 hugo -t meghna-hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 
-# Go To Public folder
-cd public
-
-# Add changes to git.
 git add .
 
 # Commit changes.
@@ -19,6 +15,17 @@ msg="rebuilding site $(date)"
 if [ -n "$*" ]; then
 	msg="$*"
 fi
+
+git commit -m "$msg"
+
+git push origin master
+
+# Go To Public folder
+cd public
+
+# Add changes to git.
+git add .
+
 git commit -m "$msg"
 
 # Push source and build repos.
